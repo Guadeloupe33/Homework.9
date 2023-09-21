40e02cebc21243c754e5a24266b8c3f899d1d18a
Guadeloupe33 <stevenfrancius@gmail.com>
2023-09-21 08:56:00 -0400

    added depencies and questions

--- script.js
+++ script.js
@@ -1,7 +1,74 @@
-// TODO: Include packages needed for this application
+// TODO: Include packages needed for this application\
+const inquirer = require ('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+.prompt(  [
+    {
+        type: 'input',
+        message: 'Name of project',
+        name: ' project',
+    },
+
+    {
+        type: 'input',
+        message: 'Name of project',
+        name: ' project',
+    },
+    {
+        type: 'input',
+        message: 'Name of project',
+        name: ' project',
+    },
+    {
+        type: 'input',
+        message: 'Name of project',
+        name: ' project',
+    },
+    {
+        type: 'input',
+        message: 'Name of project',
+        name: ' project',
+    },
+    {
+        type: 'input',
+        message: 'Name of project',
+        name: ' project',
+    },
+    {
+        type: 'input',
+        message: 'Name of project',
+        name: ' project',
+    },
+
+
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+)
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
