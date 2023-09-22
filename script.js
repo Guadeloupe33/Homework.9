44c007df6924b3b90616421eba07d07398e2d56f
Guadeloupe33 <stevenfrancius@gmail.com>
2023-09-21 21:52:27 -0400

    added back tick text

--- script.js
+++ script.js
@@ -2,7 +2,44 @@
 const inquirer = require ('inquirer');
 const fs = require('fs');
 const generateReadMe = ( {project, description, install, usage, contribution, license}) =>
-``
+`# <Project-Title:${project} >
+
+
+
+## Table of Contents 
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Description]
+- [Install]
+- [Usage]
+- [Contribution]
+- [License]
+
+## Description:${description}
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+## Installation: ${install}
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage: ${usage}
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+
+## Credits
+
+List your collaborators
+
+## License
+`
 
 // TODO: Create an array of questions for user input
 const questions = [];
